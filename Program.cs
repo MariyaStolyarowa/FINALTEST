c3510d61ee20a7e258c07f033e434e6db54300ff
Mariya <stolyarowa.mariya@yandex.ru>
2022-11-13 12:49:42 +0300

    заполняем исходный массив - вызываем метод FillArray

--- Program.cs
+++ Program.cs
@@ -61,3 +61,5 @@ void StayWithout(string[] array1, string[] array2) //Метод заполнен
     }
 }
 string[] array1 = new string[n]; //Инициализируем исходный массив
+Console.WriteLine("Введите эллементы массива:");
+FillArray(array1);
